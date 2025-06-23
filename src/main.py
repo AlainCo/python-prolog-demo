80d555b12ded7968b2cb57d5a04d20e1be49fd4c
AlainCo <13969182+AlainCo@users.noreply.github.com>
2025-06-23 14:19:39 +0200

    test pb consult

--- src/main.py
+++ src/main.py
@@ -3,13 +3,17 @@
 from prolog import PyswipUtil
 from pyswip import Prolog
 
-pyswip_util=PyswipUtil() 
-prolog = Prolog()
-pyswip_util.consult(prolog,"family.pl") # pyswip prolog.consult has a bug on Windows with \ in path
-
-
-prolog.assertz("father(michael,john)")
-print(list(prolog.query("father(michael,X)")))
-
-pyswip_util.consult(prolog,"familywith'quote'.pl") # test quote escape
-print(list(prolog.query("fatherquote(michael,X)")))
+try:
+    pyswip_util=PyswipUtil() 
+    prolog = Prolog()
+    #prolog.consult(relative_to="src",path="prolog/family.pl")
+    #prolog.consult(relative_to="src/prolog",path="family.pl")
+    #prolog.consult(path="src/prolog/family.pl")
+    pyswip_util.consult(prolog,"family.pl") # pyswip prolog.consult has a bug on Windows with \ in path
+    
+    prolog.assertz("father(michael,john)")
+    print(list(prolog.query("father(michael,X)")))
+    pyswip_util.consult(prolog,"familywith'quote'.pl") # test quote escape
+    print(list(prolog.query("fatherquote(michael,X)")))
+except Exception as e:
+    print(f"Exception: {e}")
